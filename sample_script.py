5db8e474349d7ca6669192108abd39c8a608f95b
Alex Lin <alin5250@gmail.com>
2016-12-11 14:48:10 -0500

    m

--- sample_script.py
+++ sample_script.py
@@ -8,17 +8,16 @@ network = NeuralNetwork(image_size = 28,
                         num_labels = 10,
                         batch_size = 100,
                         num_hidden_layers = 1,
-                        num_hidden_nodes = 20,
                         num_hidden_nodes = 100,
                         train_dataset = train_dataset, 
                         train_labels = train_labels, 
                         valid_dataset = valid_dataset, 
                         valid_labels = valid_labels,
-                        optimizer_type = 'ConjugateGradient',
-                        optimizer_params={'learning_rate': 0.0001, 'min_step': 0.02})
+                        optimizer_type = 'CustomGradientDescent',
+                        optimizer_params={'learning_rate': 0.5, 'min_step': 0.02})
                         #optimizer_params={'learning_rate':0.5})
 
-network.train(num_steps = 10, variable_storage_file_name = 'model0', verbose=True)
+network.train(num_steps = 100, variable_storage_file_name = 'model0', verbose=True)
 
 accuracy, _ = network.test(variable_storage_file_name = 'model0')
 print accuracy
